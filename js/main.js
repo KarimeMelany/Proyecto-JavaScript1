b04311dc5f731ab12f8d54493b62702339e625a6
Pingugou <ramirogernotmonteroperez2@gmail.com>
2024-11-19 14:24:47 -0400

    javascri3p

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let mensaje="Hola Mundo";
+console.log(mensaje);
+let numerouno=5;
+let numerodos=6;
+let numerotres=3;
+
+function sumar()
+{
+let numerouno=5;
+let numerodos=6;
+let numerotres=3;
+let resultado=numerouno+numerodos+numerotres;
+ alert("La suma es " + resultado);
+}
+function restar()
+{
+let numerouno=5;
+let numerodos=6;
+let numerotres=3;
+let resultado=numerouno-numerodos-numerotres;
+ alert("La resta es " + resultado);
+}
+function multiplicar()
+{
+let numerouno=5;
+let numerodos=6;
+let numerotres=3;
+let resultado=numerouno*numerodos*numerotres;
+ alert("La multiplicacaion es " + resultado);
+}
+function dividir()
+{
+let numerouno=5;
+let numerodos=6;
+let numerotres=3;
+let resultado=numerouno/numerodos/numerotres;
+ alert("La division es " + resultado);
+}
+
+
